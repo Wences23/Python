be72dd84efa2910d5f60d4dad31151db2e60fec0
Trapiella Ignacio Wenceslao <126634043+Wences23@users.noreply.github.com>
2023-03-30 01:45:57 -0300

    Delete git/CV final directory

--- git/CV final/main.js
+++
@@ -1,6 +0,0 @@
-/* no lo pude ejecutar desde acá tuve que compilarlo directamente en el html*/
-
-/*document.getElementById('boton').onclick = function () {
-    console.log("capturamos el evento click");
-    document.getElementById("demo").inertHTML = "Mensaje enviado con exito, gracias por escribir."; 
-} */
